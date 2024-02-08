fd3354014e3be7c17db7f1fe153b376cf575ee4d
yosdul <YosdulCS@gmail.com>
2024-02-08 20:01:37 +0000

    add options.lua

---
+++ options.lua
@@ -0,0 +1,31 @@
+vim.opt.undofile = true
+
+vim.opt.guicursor = "a:block"
+vim.opt.mouseshape = "a:block"
+vim.opt.mouse = "a"
+
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.smartindent = true
+vim.opt.breakindent = true
+vim.opt.wrap = false
+
+vim.opt.browsedir = "buffer"
+
+vim.opt.incsearch = false
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.numberwidth = 3
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
+
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+
+vim.opt.termguicolors = true
+vim.opt.signcolumn = "yes"
